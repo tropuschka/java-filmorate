bba9b83889387f16bcc21f2ad6c0a207c50e5f34
Tropuschka <tryan.u@mail.ru>
2025-06-08 18:47:31 +0300

    Создать класс User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,14 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
-public class Film {
+public class User {
     int id;
+    String email;
+    String login;
     String name;
-    String description;
-    LocalDate releaseDate;
-    Duration duration;
+    LocalDate birthday;
 }
