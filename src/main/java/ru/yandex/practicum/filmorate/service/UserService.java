a7205bb988e99b564e2bf6451b5210e39e33eef6
Tropuschka <tryan.u@mail.ru>
2025-06-20 19:14:34 +0300

    Добавить поиск общих друзей

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,29 +2,30 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exceptions.ConditionsNotMetException;
-import ru.yandex.practicum.filmorate.exceptions.DuplicatedDataException;
-import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class UserService {
-    public Set<Long> addUserFriend(UserStorage userStorage, User user, User friend) {
-        if (userStorage.findUserById(user.getId()).isEmpty()
-                || userStorage.findUserById(friend.getId()).isEmpty()) {
-            throwNotFoundException("Пользователь не существует");
+    public static Set<Long> addUserFriend(UserStorage userStorage, User user, User friend) {
+        if (userStorage.findUserById(user.getId()).isEmpty()) {
+            ExceptionService.throwNotFoundException("Пользователь " + user.getName() + " не существует");
+        }
+        if (userStorage.findUserById(friend.getId()).isEmpty()) {
+            ExceptionService.throwNotFoundException("Пользователь " + friend.getName() + " не существует");
         }
         if (user.equals(friend)) {
-            throwValidationException("Нельзя добавить в друзья себя");
+            ExceptionService.throwValidationException("Нельзя добавить в друзья себя");
         }
         if (user.getFriends() != null && friend.getFriends() != null) {
             if (user.getFriends().contains(friend.getId()) // сделала &&, чтобы в случае, если дружба односторонняя,
                     && friend.getFriends().contains(user.getId())) { // она обновлялась до двусторонней
-                throwDuplicationException("Пользователь уже добавлен");
+            ExceptionService.throwDuplicationException("Пользователь " + friend.getName()
+                        + " уже есть в списке друзей пользователя " + user.getName());
             }
         }
         user.addFriend(friend);
@@ -33,16 +34,18 @@ public class UserService {
         return user.getFriends();
     }
 
-    public Set<Long> deleteUserFriend(UserStorage userStorage, User user, User friend) {
-        if (userStorage.findUserById(user.getId()).isEmpty()
-                || userStorage.findUserById(friend.getId()).isEmpty()) {
-            throwNotFoundException("Пользователь не существует");
+    public static Set<Long> deleteUserFriend(UserStorage userStorage, User user, User friend) {
+        if (userStorage.findUserById(user.getId()).isEmpty()) {
+            ExceptionService.throwNotFoundException("Пользователь " + user.getName() + " не существует");
+        }
+        if (userStorage.findUserById(friend.getId()).isEmpty()) {
+            ExceptionService.throwNotFoundException("Пользователь " + friend.getName() + " не существует");
         }
         if (user.equals(friend)) {
-            throwValidationException("Нельзя удалить из друзей себя");
+            ExceptionService.throwValidationException("Нельзя удалить из друзей себя");
         }
         if (!(user.getFriends().contains(friend.getId()) && friend.getFriends().contains(user.getId()))) {
-            throwNotFoundException("Пользователя нет у вас в друзьях");
+            ExceptionService.throwNotFoundException("Пользователя " + friend.getName() + " нет в друзьях пользователя " + user.getName());
         }
         if (user.getFriends().contains(friend.getId())) user.deleteFriend(friend);
         if (friend.getFriends().contains(user.getId())) friend.deleteFriend(user);
@@ -50,18 +53,19 @@ public class UserService {
         return user.getFriends();
     }
 
-    private void throwValidationException(String message) {
-        log.error(message);
-        throw new ConditionsNotMetException(message);
-    }
-
-    private void throwNotFoundException(String message) {
-        log.error(message);
-        throw new NotFoundException(message);
-    }
-
-    private void throwDuplicationException(String message) {
-        log.error(message);
-        throw new DuplicatedDataException(message);
+    public static Set<Long> findSharedFriend(UserStorage userStorage, User user, User friend) {
+        if (userStorage.findUserById(user.getId()).isEmpty()) {
+            ExceptionService.throwNotFoundException("Пользователь " + user.getName() + " не существует");
+        }
+        if (userStorage.findUserById(friend.getId()).isEmpty()) {
+            ExceptionService.throwNotFoundException("Пользователь " + friend.getName() + " не существует");
+        }
+        if (user.equals(friend)) {
+            ExceptionService.throwValidationException("Для сравнения нужны два разных пользователя");
+        }
+        Set<Long> sharedFriends = user.getFriends().stream()
+                .filter(friend.getFriends()::contains)
+                .collect(Collectors.toSet());
+        return sharedFriends;
     }
 }
