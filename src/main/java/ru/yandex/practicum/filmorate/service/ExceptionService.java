d2c48367de93b5a456739405b1b594fc2ddba4ef
Tropuschka <tryan.u@mail.ru>
2025-06-22 20:01:01 +0300

    Добавить коды ответов для исключений

--- src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
@@ -1,7 +1,9 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.yandex.practicum.filmorate.exceptions.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exceptions.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
@@ -9,16 +11,19 @@ import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 @Slf4j
 @Service
 public class ExceptionService {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public static void throwValidationException(String message) {
         log.error(message);
         throw new ConditionsNotMetException(message);
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public static void throwNotFoundException(String message) {
         log.error(message);
         throw new NotFoundException(message);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public static void throwDuplicationException(String message) {
         log.error(message);
         throw new DuplicatedDataException(message);
