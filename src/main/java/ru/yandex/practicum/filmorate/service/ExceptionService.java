5f962c63bc346ca8a901f619aa6a755242e11be7
Tropuschka <tryan.u@mail.ru>
2025-06-24 20:06:32 +0300

    Постман

--- src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
@@ -2,15 +2,15 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exceptions.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 
 @Slf4j
-@Service
+@RestControllerAdvice
 public class ExceptionService {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
