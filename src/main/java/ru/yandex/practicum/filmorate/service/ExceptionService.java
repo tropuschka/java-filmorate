84daeae0cb9d9e0f6ab11e955546d2b1f5cad173
Tropuschka <tryan.u@mail.ru>
2025-06-26 19:14:32 +0300

    Изменить тип возвращаемых данных для методов

--- src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
@@ -4,35 +4,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exceptions.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 
-import java.util.Map;
-
 @Slf4j
 @RestControllerAdvice
 public class ExceptionService {
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse throwValidationException(final ConditionsNotMetException e) {
         log.error(e.getMessage());
         return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(value = NotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> throwNotFoundException(final NotFoundException e) {
+    public ErrorResponse throwNotFoundException(final NotFoundException e) {
         log.error(e.getMessage());
-        return Map.of("NotFoundException", e.getMessage());
+        return ErrorResponse.create(e, HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(value = DuplicatedDataException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> throwDuplicationException(final DuplicatedDataException e) {
+    public ErrorResponse throwDuplicationException(final DuplicatedDataException e) {
         log.error(e.getMessage());
-        return Map.of("DuplicatedDataException", e.getMessage());
+        return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
