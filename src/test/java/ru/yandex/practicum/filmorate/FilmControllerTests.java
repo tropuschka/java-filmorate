8ec13753cee45c83527ca4e040a2a34946bbeb92
Tropuschka <tryan.u@mail.ru>
2025-06-26 20:23:57 +0300

    Удалить лишние переменные

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exceptions.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
@@ -13,46 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FilmControllerTests extends FilmorateApplicationTests {
-    @BeforeAll
-    static void prepare() {
-        dislikeFilm.setName("Dislike Film");
-        filmController.create(dislikeFilm);
-        filmController.like(dislikeFilm.getId(), me.getId());
-
-        topFilm1.setName("Film 1");
-        topFilmController.create(topFilm1);
-        topFilmController.like(topFilm1.getId(), me.getId());
-
-        topFilm2.setName("Film 2");
-        topFilmController.create(topFilm2);
-
-        topFilm3.setName("Film 3");
-        topFilmController.create(topFilm3);
-        topFilmController.like(topFilm3.getId(), me.getId());
-        topFilmController.like(topFilm3.getId(), myFriend.getId());
-
-        topFilm4.setName("Film 4");
-        topFilmController.create(topFilm4);
-        topFilmController.like(topFilm4.getId(), me.getId());
-        topFilmController.like(topFilm4.getId(), myFriend.getId());
-        topFilmController.like(topFilm4.getId(), othersFriend.getId());
-
-        topFilm5.setName("Film 5");
-        topFilmController.create(topFilm5);
-
-        topFilm6.setName("Film 6");
-        topFilmController.create(topFilm6);
-
-        topFilm7.setName("Film 7");
-        topFilmController.create(topFilm7);
-
-        topFilm8.setName("Film 8");
-        topFilmController.create(topFilm8);
-
-        topFilm9.setName("Film 9");
-        topFilmController.create(topFilm9);
-    }
-
     @Test
     void createFilm() {
         Film newFilm = filmController.create(film);
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -17,27 +17,11 @@ import java.time.Month;
 @SpringBootTest
 class FilmorateApplicationTests {
 	static Film film = new Film();
-	static Film dislikeFilm = new Film();
 	static User user = new User();
-	static User me = new User();
-	static User myFriend = new User();
-	static User othersFriend = new User();
-	static User sharedFriend = new User();
 	static FilmStorage filmStorage = new InMemoryFilmStorage();
 	static UserStorage userStorage = new InMemoryUserStorage();
 	static UserController userController = new UserController(userStorage);
 	static FilmController filmController = new FilmController(filmStorage, userController);
-	static Film topFilm1 = new Film();
-	static Film topFilm2 = new Film();
-	static Film topFilm3 = new Film();
-	static Film topFilm4 = new Film();
-	static Film topFilm5 = new Film();
-	static Film topFilm6 = new Film();
-	static Film topFilm7 = new Film();
-	static Film topFilm8 = new Film();
-	static Film topFilm9 = new Film();
-	static FilmStorage topFilmStorage = new InMemoryFilmStorage();
-	static FilmController topFilmController = new FilmController(topFilmStorage, userController);
 
 	@BeforeAll
 	static void prepare() {
@@ -50,23 +34,5 @@ class FilmorateApplicationTests {
 		user.setEmail("mail@mail.ru");
 		user.setBirthday(LocalDate.of(2000, Month.JANUARY, 1));
 		user.setName("Name");
-
-		me.setLogin("Me");
-		me.setEmail("my@mail.ru");
-		userController.create(me);
-
-		myFriend.setLogin("My_Friend");
-		myFriend.setEmail("my-friend@mail.ru");
-		userController.create(myFriend);
-		userController.addFriend(me.getId(), myFriend.getId());
-
-		othersFriend.setLogin("Others_Friend");
-		othersFriend.setEmail("others-friend@mail.ru");
-		userController.create(othersFriend);
-
-		sharedFriend.setLogin("Shared_Friend");
-		sharedFriend.setEmail("shared-friend@mail.ru");
-		userController.create(sharedFriend);
-		me.addFriend(sharedFriend);
 	}
 }
