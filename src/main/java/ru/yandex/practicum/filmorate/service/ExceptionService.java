e58eec256f818916b6b1589c74bda9b8a0838bc3
Tropuschka <tryan.u@mail.ru>
2025-07-01 22:50:01 +0300

    Добавить коды ответа для исключений

--- src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ExceptionService.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exceptions.DuplicatedDataException;
@@ -15,24 +16,28 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class ExceptionService {
     @ExceptionHandler(value = ConditionsNotMetException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse throwValidationException(final ConditionsNotMetException e) {
         log.error(e.getMessage());
         return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(value = NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse throwNotFoundException(final NotFoundException e) {
         log.error(e.getMessage());
         return ErrorResponse.create(e, HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(value = NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse throwNoSuchElementException(final NoSuchElementException e) {
         log.error(e.getMessage());
         return ErrorResponse.create(e, HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(value = DuplicatedDataException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse throwDuplicationException(final DuplicatedDataException e) {
         log.error(e.getMessage());
         return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
