d950ad1bd72b8b879b46e63c46c6f1dece3e689f
tropuschka <tryan.u@mail.ru>
2025-07-21 16:14:38 +0300

    Реализовать метод findAll для бд

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -4,10 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dao.mappers.UserDbMapper;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
 
 @Repository("UserDbStorage")
 @RequiredArgsConstructor
 public class UserDbStorage implements  UserStorage {
     private final JdbcTemplate jdbc;
     private final UserDbMapper userMapper;
+
+    @Override
+    public Collection<User> findAll() {
+        String query = "SELECT * FROM users";
+        return jdbc.query(query, userMapper);
+    }
 }
