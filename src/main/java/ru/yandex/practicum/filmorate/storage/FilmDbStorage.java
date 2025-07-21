717cd6949261eefd3857c8a1b77e75cf851a49e3
tropuschka <tryan.u@mail.ru>
2025-07-21 19:40:24 +0300

    Добавить FilmDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -1,4 +1,11 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository("FilmDbMapper")
+@RequiredArgsConstructor
 public class FilmDbStorage implements FilmStorage{
+    private final JdbcTemplate jdbc;
 }
