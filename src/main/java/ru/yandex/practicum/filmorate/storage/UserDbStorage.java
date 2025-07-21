e095669fd204f430c4de18a15c293e1a2d8a0bab
tropuschka <tryan.u@mail.ru>
2025-07-21 15:52:43 +0300

    Добавить маппер пользователей для бд

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -1,7 +1,13 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.dao.mappers.UserDbMapper;
 
-@Component("UserDbStorage")
+@Repository("UserDbStorage")
+@RequiredArgsConstructor
 public class UserDbStorage implements  UserStorage {
+    private final JdbcTemplate jdbc;
+    private final UserDbMapper userMapper;
 }
