d9c1104fe24793aa7fcb5edce4545b13055ba46e
tropuschka <tryan.u@mail.ru>
2025-07-21 20:26:17 +0300

    Добавить получение всех фильмов

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -3,9 +3,20 @@ package ru.yandex.practicum.filmorate.storage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.mappers.FilmDbMapper;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
 
 @Repository("FilmDbMapper")
 @RequiredArgsConstructor
 public class FilmDbStorage implements FilmStorage{
     private final JdbcTemplate jdbc;
+    private final FilmDbMapper filmMapper;
+
+    @Override
+    public Collection<Film> findAll() {
+        String query = "SELECT * FROM films;";
+        return jdbc.query(query, filmMapper);
+    }
 }
