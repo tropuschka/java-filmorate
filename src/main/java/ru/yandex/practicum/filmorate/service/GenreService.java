4f61f57ea7587e6725fabe3bcb483accf73a1d65
tropuschka <tryan.u@mail.ru>
2025-07-22 21:28:22 +0300

    Добавить сервис для работы с жанрами

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.storage.GenreStorage;
+
+import java.util.Collection;
+
+@Slf4j
+@Component
+public class GenreService {
+    private final GenreStorage genreStorage;
+
+    @Autowired
+    public GenreService(GenreStorage genreStorage) {
+        this.genreStorage = genreStorage;
+    }
+
+    public Collection<Genre> findAll() {
+        return genreStorage.findAll();
+    }
+
+    public Genre findById(int id) {
+        return genreStorage.findGenreById(id).orElseThrow(() -> new NotFoundException("Жанр не найден"));
+    }
+}
