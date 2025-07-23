f5e33c7f5ab49a933d7ac813b3688f2794c73a98
tropuschka <tryan.u@mail.ru>
2025-07-23 18:29:54 +0300

    Postman

--- src/main/java/ru/yandex/practicum/filmorate/mappers/FilmDbMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/FilmDbMapper.java
@@ -52,7 +52,9 @@ public class FilmDbMapper implements RowMapper<Film> {
         film.setGenres(genreSet);
 
         String likesSql = "SELECT user_id FROM film_likes WHERE film_id = ?;";
-        Set<Integer> likes = new HashSet<>(jdbc.queryForList(likesSql, Integer.class, resultSet.getInt("id")));
+        List<Integer> likeList = jdbc.queryForList(likesSql, Integer.class, resultSet.getInt("id"));
+        Set<Integer> likes = new HashSet<>();
+        likes.addAll(likeList);
         film.setLikes(likes);
 
         return film;
