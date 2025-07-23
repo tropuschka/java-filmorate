f7e8dee69e5e005cc2fba01adb8a14f8b40f5f89
tropuschka <tryan.u@mail.ru>
2025-07-23 15:34:12 +0300

    CheckStyle

--- src/main/java/ru/yandex/practicum/filmorate/mappers/UserDbMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mappers/UserDbMapper.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+
 @Component("UserDbMapper")
 @RequiredArgsConstructor
 public class UserDbMapper implements RowMapper<User> {
